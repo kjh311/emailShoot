6b0c1b621df161d01c1ab9f2a6ca5f9139b82436
Kevin Huelsmann <kjh311@gmail.com>
2017-01-24 09:41:36 -0800

    setup

--- controller.js
+++ controller.js
@@ -3,8 +3,23 @@ var app = angular.module('myApp',[]);
 // MY CONTROLLER
 app.controller('myCtrl', ['$scope', function($scope) {
 
-$scope.name = 'Kevin';
-// $scope.googlekey = <%= process.env.USER_KEY %>;
+// $scope.name = 'Kevin';
+var emailAddress = prompt("Shoot Resume off to recipient", "Email Address");
+
+if(emailAddress !== null && emailAddress !== "Email Address"){
+$scope.email = emailAddress;
+console.log(emailAddress);
+}
+
+function emailShoot() {
+    var link = "mailto:"+ emailAddress +"?subject=Kevin Huelsmann Resume&body=Hello! I've attached my resume for your consideration. Kevin Huelsmann.  (505) 999-8141"
+    console.log(emailAddress);
+    window.location.href = link;
+}
+
+emailShoot();
+console.log("Hello");
+
 
 
 }]);
